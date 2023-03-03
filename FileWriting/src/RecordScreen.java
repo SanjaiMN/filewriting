53548386eca4d6e8cb4db7a869656c51fe9a5972
sanja <sanja@Sanjai_MN>
2023-03-03 21:16:51 +0530

    after

--- FileWriting/src/RecordScreen.java
+++ FileWriting/src/RecordScreen.java
@@ -1,48 +1,46 @@
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidStartScreenRecordingOptions;
-import io.appium.java_client.screenrecording.CanRecordScreen;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
-import io.appium.java_client.service.local.flags.GeneralServerFlag;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+
 import java.io.IOException;
 import java.net.URL;
-import java.time.Duration;
-import java.util.Base64;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class RecordScreen {
+public class RecordScreen 
+{
 
-    public static void recordVideo() throws IOException, InterruptedException {
+    public static void recordVideo() throws IOException, InterruptedException 
+    {
         // Set up Appium server and driver
        
-        AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"),new DesiredCapabilities());
+    	   // Set the Appium server URL and capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        caps.setCapability(MobileCapabilityType.APP, "path/to/your/app.apk");
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
 
-        // Start screen recording for 10 seconds
-        AndroidStartScreenRecordingOptions options = new AndroidStartScreenRecordingOptions()
-                .withTimeLimit(Duration.ofSeconds(10));
-        driver.startRecordingScreen(options);
+        // Create an instance of the Appium driver
+        AndroidDriver driver = new AndroidDriver(serverURL, caps);
 
-        // Stop screen recording and decode the video data
-        String base64String =  driver.stopRecordingScreen();
-        //String base64String = ((AndroidDriver<?>) castDriver()).stopRecordingScreen();
-        byte[] decodedData = Base64.getDecoder().decode(base64String);
+        // Start recording test cases
+        driver.startRecordingScreen();
 
-        // Save video to file
-        File outputFile = new File("recorded_video.mp4");
-        FileOutputStream outputStream = new FileOutputStream(outputFile);
-        outputStream.write(decodedData);
-        outputStream.close();
+        // Your test code goes here...
 
-        // Quit driver and stop Appium server
+        // Stop recording test cases
+        String videoRecording = driver.stopRecordingScreen();
+        System.out.println("The video recording of the test case can be found at: " + videoRecording);
+
+        // Quit the driver
         driver.quit();
        
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException 
+    {
         recordVideo();
     }
 }
