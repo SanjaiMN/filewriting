1c981fff83a1987196d6b294be63dbda09823904
sanja <sanja@Sanjai_MN>
2023-03-03 21:33:52 +0530

    zz

--- FileWriting/src/RecordScreen.java
+++ FileWriting/src/RecordScreen.java
@@ -13,27 +13,23 @@ public class RecordScreen
 
     public static void recordVideo() throws IOException, InterruptedException 
     {
-        // Set up Appium server and driver
-       
-    	   // Set the Appium server URL and capabilities
+    	
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
         caps.setCapability(MobileCapabilityType.APP, "path/to/your/app.apk");
         URL serverURL = new URL("http://localhost:4723/wd/hub");
 
-        // Create an instance of the Appium driver
+        // Create an instance
         AndroidDriver driver = new AndroidDriver(serverURL, caps);
 
         // Start recording test cases
         driver.startRecordingScreen();
 
-        // Your test code goes here...
+        // Your test code goes here
 
         // Stop recording test cases
         String videoRecording = driver.stopRecordingScreen();
-        System.out.println("The video recording of the test case can be found at: " + videoRecording);
-
         // Quit the driver
         driver.quit();
        
